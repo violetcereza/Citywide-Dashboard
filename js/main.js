3472582c60c8742e4cda0dc65364663cf361e266
Jasper Clarkberg <jasperclarkberg@Isope.local>
2014-06-03 10:26:17 -0400

    Started on sparks

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-  $("svg image").each(function() {
+  $("image").each(function() {
     var image = $(this);
     if (!image.attr("width") && !image.attr("height")) {
       // http://stackoverflow.com/questions/318630/get-real-image-width-and-height-with-javascript-in-safari-chrome
@@ -13,12 +13,12 @@ $(function() {
   })
   
   // http://stackoverflow.com/questions/1108480/svg-draggable-using-jquery-and-jquery-svg
-  // $("svg image, svg rect").draggable().bind('mousedown', function(event, ui){
-  //   // bring target to front
-  //   //$(event.target.parentElement).append( event.target );
-  // }).bind('drag', function(event, ui){
-  //   // update coordinates manually, since top/left style props don't work on SVG
-  //   event.target.setAttribute('x', ui.position.left);
-  //   event.target.setAttribute('y', ui.position.top-$(document.body).scrollTop());
-  // });
+  $("svg image, svg rect").draggable().bind('mousedown', function(event, ui){
+    // bring target to front
+    //$(event.target.parentElement).append( event.target );
+  }).bind('drag', function(event, ui){
+    // update coordinates manually, since top/left style props don't work on SVG
+    event.target.setAttribute('x', ui.position.left);
+    event.target.setAttribute('y', ui.position.top-$(document.body).scrollTop());
+  });
 });
